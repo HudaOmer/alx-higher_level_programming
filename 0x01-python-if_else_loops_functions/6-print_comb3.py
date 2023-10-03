bcb3509f437f88904c1e20a34275f8479c149105
HudaOmer <104313411+HudaOmer@users.noreply.github.com>
2023-10-03 23:15:28 +0300

    Solve task 6

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,1 +1,8 @@
 #!/usr/bin/python3
+for i in range(0, 8):
+    for j in range(i + 1, 10):
+        if i == j:
+            continue
+        else
+            print("{0}{1}".format(i, j), end=", ")
+print("89")
