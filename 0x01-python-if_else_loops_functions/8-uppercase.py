9071622b585cde69aed5709461e8947f8192bffe
HudaOmer <104313411+HudaOmer@users.noreply.github.com>
2023-10-04 00:03:41 +0300

    Solve task 8

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+def uppercase(str):
+    for ch in str:
+        s = 0
+        if ord(ch) in range(97, 123):
+            s = 32
+        print("{}".format(ord(ch) - 32), end='')
+     print("")
+            
