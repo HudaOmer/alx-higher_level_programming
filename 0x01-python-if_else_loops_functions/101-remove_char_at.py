395c6037c32f4440bde9b35cdcd313a25a9beeec
HudaOmer <104313411+HudaOmer@users.noreply.github.com>
2023-10-04 02:23:09 +0300

    Solve Advanced 101 concatenation

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -2,7 +2,7 @@
 def remove_char_at(str, n):
     new = ""
     j = 0
-    for i in range (0, len(str)):
+    for i in range(0, len(str)):
         if i == n:
             j += 1
             continue
