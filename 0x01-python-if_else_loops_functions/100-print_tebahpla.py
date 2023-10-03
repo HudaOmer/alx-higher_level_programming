a8311436e71c8e6d691d1526d841d03e30f45842
HudaOmer <104313411+HudaOmer@users.noreply.github.com>
2023-10-04 00:49:00 +0300

    Solve Advanced 100

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-for i in range(26, -1, -1):
+for i in range(25, -1, -1):
     if i % 2 == 0:
-        x = 97
-    else:
         x = 65
+    else:
+        x = 97
     print("{}".format(chr(i + x)), end='')
