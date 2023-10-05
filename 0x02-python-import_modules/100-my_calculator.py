c5a4367791a1dcf05002a673dc63e566c633b102
HudaOmer <hudaoe175@gmail.com>
2023-10-05 07:05:51 -0700

    Solve Advanced 100

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -4,11 +4,11 @@ if __name__ == "__main__":
     import calculator_1 as cal
     import sys
     length = len(sys.argv)
-    a = int(sys.argv[1])
-    b = int(sys.argv[3])
     if length != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
+    a = int(sys.argv[1])
+    b = int(sys.argv[3])
     if sys.argv[2] == "+":
         result = cal.add(a, b)
     elif sys.argv[2] == "-":
@@ -20,4 +20,4 @@ if __name__ == "__main__":
     else:
         print("Unknown operator. Available operators: +, -, * and /")
         exit(1)
-    print("{} {} {} = {}".format(sys.argv[1], sys.argv[2], sys.argv[3], result))
+    print("{} {} {} = {}".format(a, sys.argv[2], b, result))
