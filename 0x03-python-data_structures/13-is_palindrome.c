ce815cdff6aaa0839135ed7295e19b6dbd62474e
HudaOmer <104313411+HudaOmer@users.noreply.github.com>
2023-10-10 02:30:28 +0300

    Solve task 13

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,39 +1,67 @@
 #include "lists.h"
 
 /**
+ * reverse_listint - reverse the list
+ * @head: pointer to the list to be reversed
+ * Return: pointer to the reversedd list
+ */
+void reverse_listint(listint_t **head)
+{
+	listint_t *next = NULL, *prev = NULL;
+
+	while (*head)
+	{
+		next = (*head)->next;
+		(*head)->next = prev;
+		prev = *head;
+		*head = next;
+	}
+	*head = prev;
+}
+
+/**
  * is_palindrome - check if a linked list is palindrome
  * @head: pointer to the list
  * Return: 0 if it is not a palindrome, 1 if it is a palindrome
  */
-
 int is_palindrome(listint_t **head)
 {
-	int *p = NULL, i, count = 0, t = 0;
-	listint_t *list = *head;
+	listint_t *slow = *head, *fast = *head, *temp = *head, *dup = NULL;
 
-	if (!*head)
+	if (*head == NULL || (*head)->next == NULL)
 		return (1);
-	while (list)
-		list = list->next, t++;
 
-	p = malloc(sizeof(int) + (t + 1));
-	if (!p)
-		return (1);
-	list = *head;
-	while (t)
+	while (1)
 	{
-		*(p + count) = list->n;
-		list = list->next;
-		count++, t--;
-	}
-	for (i = 0; i < count / 2; i++)
-	{
-		if (p[i] != p[count - i - 1])
+		fast = fast->next->next;
+		if (!fast)
 		{
-			free(p);
-			return (0);
+			dup = slow->next;
+			break;
 		}
+		if (!fast->next)
+		{
+			dup = slow->next->next;
+			break;
+		}
+		slow = slow->next;
 	}
-	free(p);
-	return (1);
+
+	reverse_listint(&dup);
+
+	while (dup && temp)
+	{
+		if (temp->n == dup->n)
+		{
+			dup = dup->next;
+			temp = temp->next;
+		}
+		else
+			return (0);
+	}
+
+	if (!dup)
+		return (1);
+
+	return (0);
 }
