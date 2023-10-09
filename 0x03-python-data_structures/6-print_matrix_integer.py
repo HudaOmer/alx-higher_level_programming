bdadf4fefeaeaeb3d137d11e7ee5af46eaeca456
HudaOmer <hudaoe175@gmail.com>
2023-10-09 02:45:09 -0700

    Solve task 6

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,6 +2,10 @@
 
 def print_matrix_integer(matrix=[[]]):
     for i in matrix:
-        for j in i:
-            print(j, end = " ")
+        str = " "
+        x = len(i)
+        for j in range(x):
+            if j == x - 1:
+                str = ""
+            print("{:d}".format(i[j]), end=str)
         print("")
