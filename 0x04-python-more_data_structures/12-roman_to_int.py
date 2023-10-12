a8d62328b97f8e3121c77a8ef80ae1d0292df3a6
HudaOmer <104313411+HudaOmer@users.noreply.github.com>
2023-10-12 03:21:17 +0300

    Solve task 12

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -2,7 +2,7 @@
 def roman_to_int(roman_string):
   sum = 0
   num = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-  if not num or type(roman_string) != str:
+  if not roman_string or type(roman_string) != str:
     return 0
   for roman in reversed(roman_string):
     i = roman_string[roman]
