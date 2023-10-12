990603903496d985971ca0a6f367593c87a1d3dd
HudaOmer <104313411+HudaOmer@users.noreply.github.com>
2023-10-12 03:41:00 +0300

    Solve Advanced 100

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -2,7 +2,8 @@
 def weight_average(my_list=[]):
   if not my_list:
         return 0
-    sum, div = 0, 0
+    sum = 0
+    div = 0
     for i in my_list:
         sum += tup[0] * tup[1]
         div += tup[1]
