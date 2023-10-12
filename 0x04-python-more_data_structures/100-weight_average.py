020d08fa209d2d2eb638ee0d9a201122558be408
HudaOmer <104313411+HudaOmer@users.noreply.github.com>
2023-10-12 03:38:22 +0300

    Solve Advanced 100

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def weight_average(my_list=[]):
+  if not my_list:
+        return 0
+    sum, div = 0, 0
+    for i in my_list:
+        sum += tup[0] * tup[1]
+        div += tup[1]
+    return (num / den)
