718f06069cde9225d804e3d25921a044e23dac37
HudaOmer <104313411+HudaOmer@users.noreply.github.com>
2023-10-24 20:12:45 +0300

    Solve task 3

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""A Module that DEFINES a Square Class"""
+
+
+class Square:
+    """A Class that REPRESENTS a Square"""
+
+    def __init__(self, size=0):
+        """Initialize a Square instance with
+        a size that has a positive int value
+        Args:
+            size: a private instance attribure
+            that holds the size of the square
+        Raises:
+            TypeError: if size isn't int
+            ValueError: if size is negative"""
+
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """A Method that returns the area
+        of the square"""
+
+        return area ** 2
