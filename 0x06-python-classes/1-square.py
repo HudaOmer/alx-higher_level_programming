692af45acfd89ba7535cf31a613323969fe1a199
HudaOmer <104313411+HudaOmer@users.noreply.github.com>
2023-10-24 19:08:50 +0300

    Solve task 1

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -5,8 +5,6 @@
 class Square:
     """Class named square holds ite size"""
 
-
-
-    def __init__(self, size):
-        self.size = size
+    def __init__(self, _Square__sizee):
+        self._Square__size = _Square__sizee
         pass
