9feef068a49da03cbc8b728faef81b3b871189b3
HudaOmer <104313411+HudaOmer@users.noreply.github.com>
2023-10-24 22:15:56 +0300

    Solve task 4 rm doc

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -33,14 +33,6 @@ class Square:
 
     @size.setter
     def size(self, value):
-        """A Method that sets the size i.e
-        setter
-        Args:
-            value: to set for size
-        Raises:
-            TypeError: if size isn't int
-            ValueError: if size is negative"""
-
         if type(value) is not int:
           raise TypeError("size must be an integer")
         if value < 0:
