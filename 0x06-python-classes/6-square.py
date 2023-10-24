1f420af9ce994dd6f93853f8741c5d4d4169c757
HudaOmer <104313411+HudaOmer@users.noreply.github.com>
2023-10-24 23:21:49 +0300

    Solve task 6

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -19,8 +19,8 @@ class Square:
             raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
-        self.__size = size
-        self.__position = position
+        self.size = size
+        self.position = position
 
     @property
     def size(self):
@@ -61,7 +61,6 @@ class Square:
                 not all(num >= 0 for num in value)):
             raise TypeError("position must be a tuple of 2 positive integers")
         self.__position = value
-
 
     def area(self):
         """A Method that returns the area
