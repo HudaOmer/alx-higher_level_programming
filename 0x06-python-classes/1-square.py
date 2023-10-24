f6c5b6f46dc81904080f5402e5d0c6c69d9e0835
HudaOmer <hudaoe175@gmail.com>
2023-10-24 08:50:04 -0700

    Add files

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Class named square holds ite size"""
+
+
+class Square:
+    """Class named square holds ite size"""
+
+
+
+    def __init__(self, size):
+        self.size = size
+        pass
