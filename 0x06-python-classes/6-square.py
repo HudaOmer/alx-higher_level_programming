fdd5b93160a47bbd3d02b7e934e0dc0906537e5b
HudaOmer <104313411+HudaOmer@users.noreply.github.com>
2023-10-24 23:04:45 +0300

    Solve task 6 ?

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -84,6 +84,11 @@ class Square:
         """A Method that prints a square with #"""
 
         if self.__size == 0:
-            print()
-        for i in range(self.__size):
-            print("#" * self.__size)
+            print("")
+            return
+
+        [print("") for i in range(0, self.__position[1])]
+        for i in range(0, self.__size):
+            [print(" ", end="") for j in range(0, self.__position[0])]
+            [print("#", end="") for k in range(0, self.__size)]
+            print("")
