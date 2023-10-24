e02f8f8a917b93565d1331802091de3c594cec27
HudaOmer <104313411+HudaOmer@users.noreply.github.com>
2023-10-24 22:14:33 +0300

    Solve task 4 indents

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -23,29 +23,29 @@ class Square:
 
     @property
     def size(self):
-    """A Method that returns the size i.e
-    getter
-    Returns:
-        size of square
-    """
+        """A Method that returns the size i.e
+        getter
+        Returns:
+            size of square
+        """
 
         return self.__size
 
     @size.setter
     def size(self, value):
-      """A Method that sets the size i.e
-      setter
-      Args:
-          value: to set for size
-      Raises:
+        """A Method that sets the size i.e
+        setter
+        Args:
+            value: to set for size
+        Raises:
             TypeError: if size isn't int
             ValueError: if size is negative"""
 
-     if type(value) is not int:
-            raise TypeError("size must be an integer")
-     if value < 0:
+        if type(value) is not int:
+          raise TypeError("size must be an integer")
+        if value < 0:
             raise ValueError("size must be >= 0")
-     self.__size = value
+        self.__size = value
 
     def area(self):
         """A Method that returns the area
