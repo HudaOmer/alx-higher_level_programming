f0361a08a1512c4480f5fb872273cd248a81470e
HudaOmer <104313411+HudaOmer@users.noreply.github.com>
2024-02-13 02:47:23 +0300

    Solve task 2

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 const Argc = process.argv.length;
-if (Argc == 2)
+if (Argc === 2)
   console.log('No argument');
-else if (Argc == 3)
+else if (Argc === 3)
   console.log('Argument found');
 else
   console.log('Arguments found');
