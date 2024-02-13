065481dc36d303af9145420982a831c4203100f6
HudaOmer <104313411+HudaOmer@users.noreply.github.com>
2024-02-13 03:37:23 +0300

    Solve task 10

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,2 +1,6 @@
 #!/usr/bin/node
-console.log(`${process.argv[2]} is ${process.argv[3]}`);
+function factorial (n) {
+  return n === 0 || isNaN(n) ? 1 : n * factorial(n - 1);
+}
+
+console.log(factorial(Number(process.argv[2])));
