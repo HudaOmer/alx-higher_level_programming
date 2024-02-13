9f474352eb8c0f631c4be4d323bf1f5606e4d1a9
HudaOmer <104313411+HudaOmer@users.noreply.github.com>
2024-02-13 03:35:27 +0300

    Solve task 9

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,2 +1,6 @@
 #!/usr/bin/node
-console.log(`${process.argv[2]} is ${process.argv[3]}`);
+function add (a, b) {
+  return a + b;
+}
+
+console.log(add(Number(process.argv[2]), Number(process.argv[3])));
