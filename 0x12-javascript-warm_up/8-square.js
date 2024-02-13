1e677bf22c5fb0e1687dd742ca13d9c453fd42ee
HudaOmer <104313411+HudaOmer@users.noreply.github.com>
2024-02-13 03:31:27 +0300

    Solve task 8

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,2 +1,10 @@
 #!/usr/bin/node
-console.log(`${process.argv[2]} is ${process.argv[3]}`);
+const num = Math.floor(Number(process.argv[2]));
+if (isNaN(num)) {
+  console.log('Missing size');
+} else {
+  for (let r = 0; r < num; r++) {
+    let row = '';
+    for (let c = 0; c < num; c++) row += 'X';
+    console.log(row);
+  }
