0a3149e0367350f20bfdb20a5f5548f620fb9a09
HudaOmer <104313411+HudaOmer@users.noreply.github.com>
2024-02-18 18:03:31 +0200

    task 1 add binary

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -13,7 +13,7 @@ if __name__ == "__main__":
             charset="utf8"
         )
     cursor = connect.cursor()
-    cursor.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
+    cursor.execute("SELECT * FROM states WHERE name LIKE BINARY 'N%' ORDER BY id ASC")
     query = cursor.fetchall()
     for row in query:
         print(row)
