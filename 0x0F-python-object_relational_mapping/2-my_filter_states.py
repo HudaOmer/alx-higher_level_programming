42f8486b0a4d415d36dc8cf61df155da0d19b9d1
HudaOmer <hudaoe175@gmail.com>
2024-02-18 08:17:55 -0800

    Solve task 2

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -13,7 +13,8 @@ if __name__ == "__main__":
             charset="utf8"
         )
     cursor = connect.cursor()
-    cursor.execute("SELECT * FROM states WHERE name LIKE '{}'".format(sys.argv[4]))
+    cursor.execute("SELECT * FROM states \
+            WHERE name LIKE BINARY '{}'".format(sys.argv[4]))
     query = cursor.fetchall()
     for row in query:
         print(row)
