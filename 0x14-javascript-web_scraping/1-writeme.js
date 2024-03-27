8b99f632b328dd70f1601707e3f99ea567394420
HudaOmer <hudaoe175@gmail.com>
2024-03-26 17:53:26 -0700

    Fix files

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,9 +1,1 @@
-#!usr/bin/node
-
-const fs = require('fs');
-const file = process.argv[2];
-const string = process.argv[3];
-
-fs.writeFile(file, string, 'utf-8', error => {
-  if (error) console.log(error);
-});
+#!/usr/bin/node
