4ec6f153bb321eeebafd023942d786aaf49f7577
HudaOmer <104313411+HudaOmer@users.noreply.github.com>
2024-03-27 03:57:53 +0300

    Solve task 2

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,1 +1,8 @@
 #!/usr/bin/node
+
+const request = require('request');
+const URL = process.argv[2];
+
+request.get(URL).on('response', function (response) {
+  console.log(`code: ${response.statusCode}`);
+});
