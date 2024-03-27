797047227d39e58208281684eed19b0a8381109d
HudaOmer <hudaoe175@gmail.com>
2024-03-26 17:35:32 -0700

    Add compiler

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,8 +1,1 @@
 #!/usr/bin/node
-
-const fs = require('fs');
-const file = process.argv[2];
-
-fs.readFile(file, 'utf-8', function (error, data) {
-  console.log(error || data);
-});
---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,1 @@
+#!/usr/bin/node 2-statuscode.js 3-starwars_title.js 4-starwars_count.js 5-request_store.js 6-completed_tasks.js 100-starwars_characters.js 101-starwars_characters.js
