30cd98e38f1eb246d9afd0d884d6e75f5824a37f
HudaOmer <104313411+HudaOmer@users.noreply.github.com>
2024-03-27 03:48:22 +0300

    Solve task 1

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,1 +1,9 @@
-#!/usr/bin/node 2-statuscode.js 3-starwars_title.js 4-starwars_count.js 5-request_store.js 6-completed_tasks.js 100-starwars_characters.js 101-starwars_characters.js
+#!usr/bin/node
+
+const fs = require('fs');
+const file = process.argv[2];
+const string = process.argv[3];
+
+fs.writeFile(file, string, 'utf-8', error => {
+  if (error) console.log(error);
+});
